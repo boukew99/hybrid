8526ae818f8164b64cee864b11d0e6aade1e0735
Snamco <63397429+Snamco@users.noreply.github.com>
2022-11-15 23:15:45 +0100

    gekleurde seeds counter met bug

--- map/bouke.js
+++ map/bouke.js
@@ -20,7 +20,7 @@ function generate_map() {
 				cell.src = "map/desert.png";
 				cell.alt = "desert";
 				difficulty-= 1;
-			}
+			} 
 		}
 		localStorage.setItem("state", JSON.stringify(state))
 		return state
@@ -56,16 +56,18 @@ function main() {
 	const urlParams = new URLSearchParams(window.location.search);
 	if (urlParams.has('t')) {
 		const turn = parseInt( urlParams.get('t') )
-		document.getElementById('turn').value = turn + 1
+		growing_seed()
+		document.getElementById('turn').value = turn + 1 //waarom zet je de +turn niet in een aparte functie?
 		document.getElementById('turn_text').textContent = 'turn: ' + turn	
 		random_event_text()
 		document.getElementById("difficulty").style.display = "none"
-		set_map_state( JSON.parse(localStorage.getItem("state")) )
+		
 	}
 	else {
-		let	state = generate_map()
+	//	let	state = generate_map()
 		let forestTiles = state.filter(x => x==1).length
 		document.getElementById("difficulty").innerHTML +=Math.round(( (difficulty*100/24))) + "%";
+	//	set_map_state( JSON.parse(localStorage.getItem("state")) )
 	}
 	//random_event()
 	
@@ -73,7 +75,7 @@ function main() {
 
 }
 
-window.addEventListener('load', main)
+// window.addEventListener('load', main)
 
 // event
 event_text.setAttribute('style', 'white-space: pre;'); //this makes linebreak (/r/n ) possible.
@@ -82,5 +84,39 @@ function random_event(){
 	view.src = Math.random() > 0.5 ? 'map/sponsor.svg' : "map/fire.svg" 
 }
 
+function plantTree(){
+const row = document.createElement('div');
+const randomColor = Math.floor(Math.random()*16777215).toString(16);
+var rdm_color = "#" + randomColor;
 
+  row.className = 'row';
+	
+  row.innerHTML = '<i style ='+rdm_color+' class="fa-solid fa-seedling"></i>';
+  row.style.color = rdm_color;
+  document.getElementById('seedcount').appendChild(row);
+  }
+  
+function growing_seed(){
+	 const rows = document.getElementsByClassName('row')
+	 var test = 0;
+	 for (const row of rows){
+		test+=1;
+		console.log(test+ "heeft seeds "+row.getElementsByClassName("fa-solid fa-seedling").length);
+		if (row.getElementsByClassName("fa-solid fa-seedling").length==3){
+		row.remove()
+		console.log("destroyed "+test)}
+		
+		else
+		{row.innerHTML += '<i class="fa-solid fa-seedling"></i>';
+	console.log("added to "+test);}
+		
+	 }
+}
+var turn_count = 0;
+function next_turn(){
+	//document.getElementById('turn').value = turn + 1 //waarom zet je de +turn niet in een aparte functie?
+	turn_count+=1;
+	document.getElementById('turn_text').textContent = 'turn: ' + turn_count	;
+	growing_seed();
+}
 
